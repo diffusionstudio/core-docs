25c9064836cf0962b3ac7389e829702820657afe
konstantin-paulus <mail@konstantinpaulus.de>
2025-02-18 16:05:46 +0100

    added version 3

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -1,5 +1,19 @@
 const meta = {
   index: "",
+  terminology: "Terminology",
+  composition: "Composition",
+  layers: "Layers",
+  clips: "Clips",
+  rendering: "Rendering",
+  'keyframe-animations': "Keyframe Animations",
+  masks: "Masks",
+  timestamp: "Timestamp",
+  events: "Events",
+  transcript: "Transcript",
+  '###': {
+    type: 'separator',
+    title: 'Legacy',
+  },
   "version-1": "Version 1",
   "version-2": "Version 2",
 };
